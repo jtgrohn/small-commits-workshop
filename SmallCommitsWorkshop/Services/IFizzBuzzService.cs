a454aad38e4b43be34dd0f3d741b4b29b539c3a8
Joshua Grohn <Joshua.Groen@D2L.com>
2018-11-06 16:14:01 -0500

    set default values for fizz and buzz, allow for overides in fizz buzz service

--- SmallCommitsWorkshop/Services/IFizzBuzzService.cs
+++ SmallCommitsWorkshop/Services/IFizzBuzzService.cs
@@ -1,5 +1,5 @@
 ﻿namespace SmallCommitsWorkshop.Services {
 	public interface IFizzBuzzService {
-		string Calculate( int number );
+		string Calculate( int number, string fizz = "Fizz", string buzz = "Buzz" );
 	}
 }
